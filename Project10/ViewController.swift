0337749f717e4e2e629c75c490cf709d41b8db3c
Saurabh <saurabh.agarwal@mxplayer.in>
2022-10-11 10:20:34 +0530

    adding image picker controller

--- Project10/ViewController.swift
+++ Project10/ViewController.swift
@@ -14,11 +14,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        // Do any additional setup after loading the view.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+        
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
-        layout.minimumLineSpacing = 1
-        layout.minimumInteritemSpacing = 1
         layout.itemSize = CGSize(width: 140, height: 180)
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         
@@ -29,14 +29,39 @@ class ViewController: UIViewController {
         collectionView.dataSource = self
         collectionView.delegate = self
         
+        collectionView.backgroundColor = .black
+        
         view.addSubview(collectionView)
         
         collectionView.frame = view.bounds
     }
+    
+    @objc func addNewPerson() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        present(imagePicker, animated: true)
+    }
 }
 
-extension ViewController: UICollectionViewDelegate {
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appending(path: imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        dismiss(animated: true)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
 }
 
 extension ViewController: UICollectionViewDataSource {
@@ -46,11 +71,18 @@ extension ViewController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PersonCell.identifier, for: indexPath) as! PersonCell
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PersonCell.identifier,
+                                                            for: indexPath) as? PersonCell else {
+            return UICollectionViewCell()
+        }
+        cell.backgroundColor = .white
         cell.config(label: "Cell \(indexPath.row + 1)")
         return cell
     }
     
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
     }
